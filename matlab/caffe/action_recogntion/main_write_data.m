369327cca427d0d8f3820e1800436e8da8ef16e0
shusain <farzadhusain@gmail.com>
2014-09-25 19:54:33 +0200

    added function to write test files

--- matlab/caffe/action_recogntion/main_write_data.m
+++ matlab/caffe/action_recogntion/main_write_data.m
@@ -1,9 +1,9 @@
 clc, close all
 
 %% determine data paths
-params.afspath = '/home/shusain/Desktop/action_recognition_spatio_temporal_features/';
-params.infopath = [params.afspath, 'action_youtube_naudio/']; 
-params.avipath{1} = [params.afspath, 'action_youtube_naudio/'];
+params.afspath = '/media/shusain/farzad_hdd/ExternalProjects/action_recognition_spatio_temporal_features/';
+params.infopath = [params.afspath, 'action_youtube_naudio/vids/']; 
+params.avipath{1} = [params.afspath, 'action_youtube_naudio/vids/'];
 params.path_img_write = '../../../data/action_youtube_naudio/';
 
 test_group=1;
@@ -16,32 +16,32 @@ TIME_SAMPLES = 100;
 size_idx = 1;
 img = [];
 %% load the train videos and save them in image format for the deep network
-m = length(all_train_files);
-valid_files = false(m,1);
-for i=1:m
-	M = load_movie_clip([params.avipath{size_idx}, char(all_train_files(i))]);
-	nFrames = size(M,3);
-	if (nFrames>100)
-		ff = int32(linspace(1,nFrames,TIME_SAMPLES));
-		for j=1:TIME_SAMPLES
-			img = cat(2,img, imresize(M(:,:,ff(j)), [ROWS COLUMNS]));
-	        end
-        	valid_files(i) = 1;
-		imwrite(img, [params.path_img_write, all_train_files{i},'.jpg']);
-	end
-	img=[];
-end
-%% write file names along with their labels
-all_train_files = all_train_files(valid_files);
-
-for i=1:length(all_train_files)
-	all_train_files{i} = ['/home/shusain/caffe/data/action_youtube_naudio/', all_train_files{i},'.jpg'];
-end
-
-all_train_labels = all_train_labels(valid_files);
-
-T = table(all_train_files,all_train_labels);
-writetable(T,[params.path_img_write, 'train.txt'],'WriteVariableNames',false,'Delimiter',' ');
+% m = length(all_train_files);
+% valid_files = false(m,1);
+% for i=1:m
+% 	M = load_movie_clip([params.avipath{size_idx}, char(all_train_files(i))]);
+% 	nFrames = size(M,3);
+% 	if (nFrames>100)
+% 		ff = int32(linspace(1,nFrames,TIME_SAMPLES));
+% 		for j=1:TIME_SAMPLES
+% 			img = cat(2,img, imresize(M(:,:,ff(j)), [ROWS COLUMNS]));
+% 	        end
+%         	valid_files(i) = 1;
+% 		imwrite(img, [params.path_img_write, all_train_files{i},'.jpg']);
+% 	end
+% 	img=[];
+% end
+% %% write train file names along with their labels
+% all_train_files = all_train_files(valid_files);
+% 
+% for i=1:length(all_train_files)
+% 	all_train_files{i} = ['/home/shusain/caffe/data/action_youtube_naudio/', all_train_files{i},'.jpg'];
+% end
+% 
+% all_train_labels = all_train_labels(valid_files);
+% 
+% T = table(all_train_files,all_train_labels);
+% writetable(T,[params.path_img_write, 'train.txt'],'WriteVariableNames',false,'Delimiter',' ');
 
 %% load the test videos and save them in image format for the deep network
 m = length(all_test_files);
@@ -59,10 +59,10 @@ for i=1:m
         end
         img=[];
 end
-%% write file names along with their labels
-all_test_files = all_train_files(valid_files);
+%% write test file names along with their labels
+all_test_files = all_test_files(valid_files);
 
-for i=1:length(all_train_files)
+for i=1:length(all_test_files)
         all_test_files{i} = ['/home/shusain/caffe/data/action_youtube_naudio/', all_test_files{i},'.jpg'];
 end
 
